ecd5f75454fa346d4cdd0eb73118b8c17c59ae27
Forge Keeper <135458725+Forge-Keeper@users.noreply.github.com>
2026-08-24 12:53:55 -0300

    feat: export executable delta contracts

---
+++ src/olist_data_platform/platform/delta/__init__.py
@@ -0,0 +1,17 @@
+from olist_data_platform.platform.delta.contract import (
+    BRONZE_INGESTION_TIMESTAMP,
+    ColumnContract,
+    DatasetContract,
+    SchemaEvolutionPolicy,
+    TableLayout,
+    TableMetadata,
+)
+
+__all__ = [
+    "BRONZE_INGESTION_TIMESTAMP",
+    "ColumnContract",
+    "DatasetContract",
+    "SchemaEvolutionPolicy",
+    "TableLayout",
+    "TableMetadata",
+]
